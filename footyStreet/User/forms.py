83ea87e73ce2063cc43afc369ed100f5d0afb5a3
DniGaytan <dnigaytan07@gmail.com>
2018-12-05 01:46:46 -0600

    Se modificaron los templates de login y register

--- footyStreet/User/forms.py
+++ footyStreet/User/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget = forms.PasswordInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget = forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'contraseña'}))
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
         widgets = {
-        	'username': forms.TextInput(attrs={'class':'form-control'}),
-        	'email': forms.TextInput(attrs={'class':'form-control'}),
+        	'username': forms.TextInput(attrs={'class':'form-control', 'placeholder':'usuario'}),
+        	'email': forms.TextInput(attrs={'class':'form-control', 'placeholder':'correo'}),
 }
